764250de42b33021c930ba9415af8c0d9d23caec
Baxter <Baxter@DESKTOP-35GI3G1>
2016-11-04 20:54:24 -0500

    Add project files.

---
+++ LineSampleBrianMc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LineSampleBrianMc.Startup))]
+namespace LineSampleBrianMc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
